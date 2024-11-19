40bbb1cf31de946c1b57a5df21530ca1dcc05773
mariathv <naeemmaria60@gmail.com>
2024-11-19 21:07:38 +0500

    view booking for service provider working + logout functioanality

---
+++ application/controllers/bookingItemController.java
@@ -0,0 +1,62 @@
+package application.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class bookingItemController {
+
+    @FXML
+    private HBox sampleHBOX;
+
+    @FXML
+    private Text userID;
+    @FXML
+    private Text username;
+    @FXML
+    private Text bookingDate;
+    @FXML
+    private Text price;
+
+    public HBox getSampleHBOX() {
+        return sampleHBOX;
+    }
+
+    public void setUserIDText(String text) {
+        userID.setText(text);
+    }
+
+    public void setUsernameText(String text) {
+        username.setText(text);
+    }
+
+    public void setBookingDateText(String text) {
+        bookingDate.setText(text);
+    }
+
+    public void setPriceText(String text) {
+        price.setText(text);
+    }
+
+    @FXML
+    public void initialize() {
+        userID.setWrappingWidth(80);
+        username.setWrappingWidth(80);
+        bookingDate.setWrappingWidth(80);
+        price.setWrappingWidth(80);
+    }
+
+    public void setData(int id, String name, String date, String cost) {
+        userID.setText(String.valueOf("   " + id));
+        userID.setWrappingWidth(80);
+        username.setText(name);
+        username.setWrappingWidth(80);
+        bookingDate.setText(date);
+        bookingDate.setWrappingWidth(90);
+        price.setText(cost);
+        price.setWrappingWidth(80);
+
+    }
+
+}
